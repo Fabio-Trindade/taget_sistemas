c98537cfc60ff5ed8aa2998e44abbc2ef7be762f
Fábio Trindade <fabiotrindadefer@gmail.com>
2023-04-12 18:22:13 -0300

    Add(+): Implementação questão 5

---
+++ questao5/string.py
@@ -0,0 +1,16 @@
+class String:
+    def __init__(self,string:str):
+        self._string = string
+    def inverterString(self) -> str:
+        newString = ""
+        tamString = len(self._string)
+        for i in range(tamString):
+            newString +=self._string[tamString-i-1]
+        return newString
+
+if __name__ == "__main__":
+    entrada = input()
+    string = String(entrada)
+    
+    print(f'String: {entrada} \nString invertida: {string.inverterString()}')
+    
